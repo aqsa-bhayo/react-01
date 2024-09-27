863c9415b4fa957f9633fdd2b67a6261d2a6849a
Aqsa-Ali-ally <aqsaalibhayo786>
2024-09-27 11:35:55 +0100

    forst commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './footer';
+import Header from './header';
+import HeroSection from './heroSection';
+
+function App() {
+  return (
+    <div className="App">
+      < Header />
+      < Footer />
+      < HeroSection />
+    </div>
+  );
+}
+
+
+export default App;
